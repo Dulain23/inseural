01fa0f9515f6bb5e356aeece17b71bab1ba7d4be
DulainRajapakse <121152566+DulainRajapakse@users.noreply.github.com>
2024-10-20 08:36:16 +0100

    V2.1

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -67,7 +67,7 @@ function Home() {
         <div className="relative hidden w-0 flex-1 lg:block">
           <img
             alt=""
-            src="https://images.unsplash.com/photo-1496917756835-20cb06e75b4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
+            src="/background.jpeg"
             className="absolute inset-0 h-full w-full object-cover"
           />
         </div>
